88b087b0ecce0218dab6302638c14efd5d091817
Mari-Anne Eerikäinen <mari-anne.eerikainen@awacademy.training>
2023-01-02 16:49:53 +0200

    front page text

--- social-media-frontend/src/components/HomePage.jsx
+++ social-media-frontend/src/components/HomePage.jsx
@@ -16,7 +16,10 @@ function HomePage() {
         <Col sm={{ span: 12 }} lg={{ span: 6 }}>
           {user.id && (
             <span title="go to profile page">
-              <Link className="link" to={`/profile/${user.id}`}>
+              <Link
+                className="link front-page-button"
+                to={`/profile/${user.id}`}
+              >
                 <h1 className="interactive box">profile</h1>
               </Link>
             </span>
@@ -26,7 +29,7 @@ function HomePage() {
         {user.id && (
           <Col sm={{ span: 12 }} lg={{ span: 6 }}>
             <span title="go to user list">
-              <Link className="link" to="/users">
+              <Link className="link front-page-button" to="/users">
                 <h1 className="interactive box">users</h1>
               </Link>
             </span>
@@ -35,17 +38,32 @@ function HomePage() {
       </Row>
       <Row className="">
         <Col className="box text card">
-          This website is a practice project for React Redux, Node.js and Web
-          Socket technology. Feel free to browse around and if you have any
-          questions or suggestions, contact me through
-          <a
-            className="link"
-            href="https://www.linkedin.com/in/mari-anne-eerik%C3%A4inen/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
+          <p>
+            This website is a practice project for React Redux, Node.js and Web
+            Socket technology. Feel free to browse around and if you have any
+            questions or suggestions, contact me through
+            <a
+              className="link front-page-link"
+              href="https://www.linkedin.com/in/mari-anne-eerik%C3%A4inen/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            While you're at it, go and check out the
+            <a
+              className="link front-page-link"
+              href="https://github.com/maeeri/SocialMediaProject"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              GitHub repo
+            </a>
+          </p>
         </Col>
       </Row>
       <Row></Row>
